5ef3ef42cad3180464a185744a3bc370d00ee9ef
IYarosh <iaros1298@gmail.com>
2026-08-28 21:39:27 +0300

    Added focus shortcuts

--- src/features/widgets-list/index.tsx
+++ src/features/widgets-list/index.tsx
@@ -1,11 +1,14 @@
 import { NavLink } from "react-router";
+import { FocusOnCtrlKey } from "shared/ui";
 
 export function WidgetsList() {
   return (
     <div className="">
       <ul>
         <li className="text-left flex flex-col gap-2">
-          <NavLink className="cursor-pointer" to="/widgets/wordle">Wordle</NavLink>
+          <FocusOnCtrlKey keyCode="ArrowRight">
+            <NavLink className="cursor-pointer" to="/widgets/wordle">Wordle</NavLink>
+          </FocusOnCtrlKey>
           <NavLink className="cursor-pointer" to="/widgets/sentence-fill">Sentence Fill</NavLink>
           <NavLink className="cursor-pointer" to="/widgets/sentence-create">Sentence Create</NavLink>
         </li>
--- src/pages/widgets/index.tsx
+++ src/pages/widgets/index.tsx
@@ -1,9 +1,15 @@
 import { WidgetsList } from "features/widgets-list";
+import { KeyboardShortcut } from "shared/ui";
 
 export function Widgets() {
   return (
     <div className="flex flex-col gap-2">
-      <h1 className="text-2xl font-bold">Widgets</h1>
+      <div className="flex items-center gap-2">
+        <h1 className="text-2xl font-bold">
+          Widgets
+        </h1>
+        <KeyboardShortcut keys={['ctrl', 'ArrowRight']} />
+      </div>
       <WidgetsList />
     </div>
   );

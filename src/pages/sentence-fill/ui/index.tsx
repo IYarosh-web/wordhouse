4ea2952eef86beb45222970cd95073d9ab96ac42
IYarosh <iyarosh@qulix.com>
2026-08-31 16:27:41 +0300

    Minor UI fix

--- src/pages/sentence-fill/ui/index.tsx
+++ src/pages/sentence-fill/ui/index.tsx
@@ -32,16 +32,17 @@ export function SentenceFillWidget() {
       <span>Complete the sentence:</span>
       <span>{sentenceToFill}</span>
       <span>{message}</span>
-      <form onSubmit={submitGuess}>
+      <form className="flex gap-2" onSubmit={submitGuess}>
         <Input
           name="guess"
           placeholder="Enter your guess"
           value={input}
           onChange={changeInput}
         />
-        <Button type="submit">Submit</Button>
+        <Button className="px-2 py-1" type="submit">Submit</Button>
+        <Button className="px-2 py-1" type="button">Hint</Button>
       </form>
-      <Button onClick={() => restart()}>Restart</Button>
+      <Button className="px-2 py-1" onClick={() => restart()}>Restart</Button>
     </div>
   );
 }
